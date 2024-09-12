8e0c5719adf0580dc71407a045b3d36a3b6e53ad
weirdo123234 <charlesarieger@icloud.com>
2024-09-11 19:34:16 -0600

    Create main.bash

---
+++ main.bash
@@ -0,0 +1,35 @@
+echo "Welcome to iwillneverbeabletowriteadylibware!"
+sleep(2)
+echo 'Oh shit, I forgot to add the code to your computer!'
+sleep(1)
+echo 'Welcome to iwillneverbeabletowriteadylibware!'
+sleep(1)
+echo "We don't have a dylib!"
+sleep(1)
+echo 'Downloading malicious software aka iwillneverbeabletowriteadylibware.'
+sleep(0.1)
+a="kdflgjdfgljdfkgdlfjgdflgdfjlkgdfjklgf"
+b="jesdfldsjflsdlkjfdslfjdslfkdsjlfkldsjfjs"
+c="aksdjlasdjaksdkjasldjkhjyiu5ytiry"
+d="asdlkas;ktuiyiurut67iyerhkdjgbnrtgiyeu5hrjnthrkegutrm5"
+echo $a
+sleep(.1)
+echo $b
+sleep(.1)
+echo $c
+sleep(.1)
+echo $d
+sleep(.5)
+echo 'Oh shit I was sleeping!'
+sleep(1)
+echo 'Downloading malicious software aka iwillneverbeabletowriteadylibware.'
+sleep(1)
+echo 'Shit it cant unzip.'
+sleep(1)
+echo 'iwillneverbeabletowriteadylibware has been downloaded!'
+sleep(1)
+open -a Discord
+sleep(0.5)
+open -a Roblox
+sleep(0.5)
+open -a Macsploit
